ddc37d669ad9c7a3394a3f70495b228fd02ea9f1
Thanura Rashmika <thanurar@ceyentra.com>
2019-06-22 01:23:44 +0530

    getAll Items

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {DataService} from '../services/data.service';
 
 @Component({
-  selector: 'app-items',
-  templateUrl: './items.component.html',
-  styleUrls: ['./items.component.scss']
+    selector: 'app-items',
+    templateUrl: './items.component.html',
+    styleUrls: ['./items.component.scss']
 })
 export class ItemsComponent implements OnInit {
 
-  constructor() { }
+    itemForm: FormGroup;
 
-  ngOnInit() {
-  }
+    items: Array<any>;
+
+    constructor(private formBuilder: FormBuilder, private dataService: DataService) {
+        this.itemForm = this.formBuilder.group({
+            txtName: [
+                '', Validators.required,
+            ],
+            txtPrice: [
+                '', Validators.required,
+            ],
+            txtQty: [
+                '', Validators.required
+            ]
+        })
+    }
+
+    ngOnInit() {
+        this.getAllItems();
+    }
+
+    getAllItems() {
+        this.dataService.getAllItems().subscribe(
+            (response: any[]) => {
+                this.items = response;
+            }
+        )
+    }
+
+    addItem() {
+        const item = {
+            code: 0,
+            name: this.itemForm.controls['txtName'].value,
+            price: this.itemForm.controls['txtPrice'].value,
+            qty: this.itemForm.controls['txtQty'].value
+        };
+
+        this.dataService.addNewItem(item).subscribe(
+            (response) => {
+
+            }
+        )
+    }
 
 }
