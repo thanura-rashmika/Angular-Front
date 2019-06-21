b524c4f93a72fad9de52f008a6926caab4c9ae91
Thanura Rashmika <thanurar@ceyentra.com>
2019-06-22 01:30:59 +0530

    Add New Item

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {DataService} from '../services/data.service';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-items',
@@ -10,8 +11,15 @@ import {DataService} from '../services/data.service';
 export class ItemsComponent implements OnInit {
 
     itemForm: FormGroup;
-
     items: Array<any>;
+
+    code: any;
+    name: any;
+    price: any;
+    qty: any;
+
+    update: boolean = false;
+    state: string = 'Add';
 
     constructor(private formBuilder: FormBuilder, private dataService: DataService) {
         this.itemForm = this.formBuilder.group({
@@ -24,7 +32,7 @@ export class ItemsComponent implements OnInit {
             txtQty: [
                 '', Validators.required
             ]
-        })
+        });
     }
 
     ngOnInit() {
@@ -36,7 +44,7 @@ export class ItemsComponent implements OnInit {
             (response: any[]) => {
                 this.items = response;
             }
-        )
+        );
     }
 
     addItem() {
@@ -46,12 +54,42 @@ export class ItemsComponent implements OnInit {
             price: this.itemForm.controls['txtPrice'].value,
             qty: this.itemForm.controls['txtQty'].value
         };
-
-        this.dataService.addNewItem(item).subscribe(
-            (response) => {
-
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'You are about to add a New Item!',
+            type: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, Add it!',
+            cancelButtonText: 'No, Discard it'
+        }).then((result) => {
+            if (result.value) {
+                this.dataService.addNewItem(item).subscribe(
+                    (response) => {
+                        if (response.code === 201) {
+                            Swal.fire(
+                                'Added!',
+                                'New Item has been Saved Successfully.',
+                                'success'
+                            );
+                            this.getAllItems();
+                            this.clearForm();
+                        }
+                    }
+                );
+            } else if (result.dismiss === Swal.DismissReason.cancel) {
+                Swal.fire(
+                    'Cancelled',
+                    'Item Not Added',
+                    'error'
+                );
             }
-        )
+        });
+    }
+
+    clearForm() {
+        this.itemForm.controls['txtName'].setValue('');
+        this.itemForm.controls['txtPrice'].setValue('');
+        this.itemForm.controls['txtQty'].setValue('');
     }
 
 }
